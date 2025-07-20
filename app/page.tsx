2111c78c462ba4627b341b23aa47a83f576206cd
Imran Shah <imranshah10140@gmail.com>
2025-07-20 23:20:42 +0530

    Tailwind check

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>Imran Shah Check</h1>
-      <p>Make the layout</p>
+      <h1 className="bg-red-300">Imran Shah Check</h1>
+      <p className="bg-amber-200">Make the layout</p>
     </>
   );
 }
