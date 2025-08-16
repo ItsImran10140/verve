c46d90272452f40286ebdd9805e5f2d91a9f5242
Imran Shah <imranshah10140@gmail.com>
2025-08-16 11:27:34 +0530

    fix:Change Name: Change the element tpye from div to h1 in top nan bar component

--- app/components/TopNavBar/TopNavBar.tsx
+++ app/components/TopNavBar/TopNavBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TopNavBar = () => {
-  return <div className="text-white">TopNavBar</div>;
+  return (
+    <div className="text-white">
+      <h1>Top Bar Component</h1>
+    </div>
+  );
 };
 
 export default TopNavBar;
